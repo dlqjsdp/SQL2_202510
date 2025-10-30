9bbf4793e99506820152858cb6c514cafe0f4bad
dlqjsdp <dlqjsdp224@gmail.com>
2025-10-30 17:34:39 +0900

    20251030

---
+++ 20251030/comment.sql
@@ -0,0 +1,8 @@
+select * from CODE_MASTER
+;
+
+
+alter table CODE_MASTER modify reg_dt int comment '날짜'
+;
+
+commit;
---
+++ 20251030/where1=1.sql
@@ -0,0 +1,16 @@
+select * from USER_TB
+where 1=1
+;
+
+
+update USER_TB set pwd = "12345"
+where 1=1 
+
+;
+
+delete from User_tb 
+where user_seq = user_seq
+;
+
+
+rollback
